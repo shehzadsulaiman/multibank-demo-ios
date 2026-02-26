9e59075322f8f1a0165e2cef02cc2945abca3518
shehzadsulaiman <shehzadsulaiman@gmail.com>
2026-02-26 22:00:15 +0400

    shehzad initial commit

---
+++ multibank/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  multibank
+//
+//  Created by sam on 26/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ multibankTests/multibankTests.swift
@@ -0,0 +1,16 @@
+//
+//  multibankTests.swift
+//  multibankTests
+//
+//  Created by sam on 26/02/2026.
+//
+
+import Testing
+
+struct multibankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
